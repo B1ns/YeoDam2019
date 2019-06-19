65c779f7ba898f90f76ca6898a2b1a42c9fd3d27
B1ns <qlsl3574@naver.com>
2019-06-19 19:21:08 +0900

    add InfoActivity

---
+++ app/src/main/java/com/B1ns/yeodam2019/view/activity/InfoActivity.kt
@@ -0,0 +1,13 @@
+package com.B1ns.yeodam2019.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.B1ns.yeodam2019.R
+
+class InfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
