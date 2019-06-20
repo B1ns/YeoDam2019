92effaba45f7568eb3b3fbe80d5a1959747541cf
B1ns <qlsl3574@naver.com>
2019-06-20 22:58:32 +0900

    Update Info

--- app/src/main/java/com/yeodam/yeodam2019/view/activity/InfoActivity.kt
+++ app/src/main/java/com/yeodam/yeodam2019/view/activity/InfoActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.WindowManager
 import com.yeodam.yeodam2019.R
 import com.yeodam.yeodam2019.toast
 import kotlinx.android.synthetic.main.activity_info.*
@@ -16,19 +17,17 @@ class InfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
-
         Button()
         setNickName()
+
     }
 
     private fun setNickName() {
         info_btn.setOnClickListener {
-            if (nickName.text.toString().isNotEmpty()){
+            if (nickName.text.toString().isNotEmpty()) {
                 // pref 설정
 
                 startActivity<MainActivity>()
-            }else{
-                toast("닉네임을 입력해주세요 !")
             }
         }
     }
@@ -38,21 +37,31 @@ class InfoActivity : AppCompatActivity() {
         nickName.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 // 입력이 끝났을 떄 호출
-                info_btn.visibility = View.VISIBLE
+
+                if (nickName.text.toString().isNotEmpty()) {
+                    info_btn.visibility = View.VISIBLE
+                } else {
+                    info_btn.visibility = View.GONE
+                    noinfo_btn.visibility = View.VISIBLE
+                }
 
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // 입력 하기전 호출
-
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 // 입력되는 텍스트에 변화가 있을 때
+
                 noinfo_btn.visibility = View.GONE
             }
 
         })
+
+        noinfo_btn.setOnClickListener {
+            toast("닉네임을 입력해주세요 !")
+        }
     }
 
 
