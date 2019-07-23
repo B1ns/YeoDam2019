44be669710faf3645c865c3dfca89d4c5f23070c
B1ns <qlsl3574@naver.com>
2019-07-23 15:59:35 +0900

    add ServiceActivity

---
+++ app/src/main/java/com/yeodam/yeodam2019/view/activity/setting/ServiceActivity.kt
@@ -0,0 +1,13 @@
+package com.yeodam.yeodam2019.view.activity.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yeodam.yeodam2019.R
+
+class ServiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service)
+    }
+}
