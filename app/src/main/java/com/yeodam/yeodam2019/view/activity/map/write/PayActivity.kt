b254ed28a77dbfd26246c96cb56ccecceb955579
B1ns <qlsl3574@naver.com>
2019-07-30 11:53:43 +0900

    Update Package

--- app/src/main/java/com/yeodam/yeodam2019/view/activity/map/write/PayActivity.kt
+++ app/src/main/java/com/yeodam/yeodam2019/view/activity/map/write/PayActivity.kt
@@ -1,13 +1,13 @@
-package com.yeodam.yeodam2019.view.activity
+package com.yeodam.yeodam2019.view.activity.map.write
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yeodam.yeodam2019.R
 
-class memoActivity : AppCompatActivity() {
+class PayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo)
+        setContentView(R.layout.activity_pay)
     }
 }
--- app/src/main/java/com/yeodam/yeodam2019/view/activity/map/write/memoActivity.kt
+++ app/src/main/java/com/yeodam/yeodam2019/view/activity/map/write/memoActivity.kt
@@ -1,12 +1,13 @@
-package com.yeodam.yeodam2019
+package com.yeodam.yeodam2019.view.activity.map.write
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yeodam.yeodam2019.R
 
-class PayActivity : AppCompatActivity() {
+class memoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pay)
+        setContentView(R.layout.activity_memo)
     }
 }
